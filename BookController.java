078c9cf4437e3c3dce88d5fdf3db9ba405c532e0
Dycoles <134562267+Dycoles@users.noreply.github.com>
2024-10-03 21:59:05 -0700

    Update BookController.java
    updated getBooksFromLibrary

--- BookController.java
+++ BookController.java
@@ -1,41 +1,47 @@
 package bookLibrary;
 
 public class BookController {
-	
+
 	private BookModel myModel;
-	
+
 	public BookController(BookModel model) {
 		myModel = model;
 	}
-	
-	public void searchLibrary() {
-		//myModel.search();
+
+	public void searchLibrary(int searchBy, String search) {
+		 myModel.search(searchBy, search);
 	}
-	
+
 	public void addBookToLibrary(Book book) {
 		myModel.addBook(book);
 	}
-	
+
 	public void readBook(Book book) {
 		myModel.setToRead(book);
 	}
-	
+
 	public void rateBook(Book book, int rating) {
 		myModel.rate(book, rating);
 	}
-	
-	public void getBooksFromLibrary() {
-		myModel.getBooks();
+
+	public void getBooksFromLibrary(int searchBy) {
+		if (searchBy == 0) {
+			myModel.getBooks(searchBy);
+		} else if (searchBy == 1) {
+			myModel.getBooks(searchBy);
+		} else if (searchBy == 2){
+			myModel.getBooks(searchBy);
+		} else {
+			myModel.getBooks(searchBy);
+		}
 	}
-	
+
 	public String[] suggestBook() {
-		
-		Book suggestion=myModel.suggestRead();
-		String [] bookInfo= {suggestion.getTitle(), suggestion.getAuthor()};
+		Book suggestion = myModel.suggestRead();
+		String[] bookInfo = { suggestion.getTitle(), suggestion.getAuthor() };
 		return bookInfo;
 	}
-	
-	
+
 	public void addListOfBooks(String fileName) {
 		myModel.addBooks(fileName);
 	}
